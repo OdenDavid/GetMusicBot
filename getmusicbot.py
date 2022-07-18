719bc6a31340cb34fe35d67c07220a6de1b953fd
David Oden <davidodenikpi@gmail.com>
2022-07-18 09:21:47 +0100

    Applied all rules for reply

--- getmusicbot.py
+++ getmusicbot.py
@@ -39,13 +39,38 @@ while True:
         #print("Bro what's good!")
         #print(f"{mention.author.screen_name} - {mention.text}")
         mention_id = mention.id
-        if mention.in_reply_to_status_id is not None and mention.author.id != bot_id:
+        if mention.in_reply_to_status_id is not None and mention.author.id != bot_id: # Check if it reply to a tweet, and not from this bot
+            
+            # 'entities': {'description': {'urls': []}}
+            # 'media_url_https': 'https://pbs.twimg.com/media/FXGjNkXXgAEmixR.jpg'
+            #print(mention)
+            tweet_above = api.get_status(mention.in_reply_to_status_id)
+            print(tweet_above)
+            if 'media_url_https' not in str(tweet_above): # If there is no media in the tweet at all
+                pass # No media
+            else:
+                for medium in tweet_above.extended_entities['media']:
+                    if medium['type'] != 'video': # If the media type is not a video
+                        pass # Not a video
+                    else:
+                        print("A video")
+                        video_url = medium['video_info']['variants'][1]['url']
+
+                        with open("Mentions.txt", "a") as textfile:
+                            textfile.write(mention.author.screen_name+":"+mention.id_str+":"+video_url)
+
+            """likelytexts = ["spotify", "apple music", "apple_music"]
+
+            if any(text in mention.text.lower().replace("@getmusicbot", "") for text in likelytexts):
+                print(test.lower().replace("@getmusicbot", ""))
+            
+            
             try:
                 print("Attempting to reply")
                 api.update_status(message.format(mention.author.screen_name), in_reply_to_status_id=mention.id_str)
                 print("Replied fine")
             except Exception as e:
-                print(e)
+                print(e)"""
         else:
             pass
     time.sleep(15)
